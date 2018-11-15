c3ceb2c4ba72b2d21698d8905ada908c0e8dc036
Alisher12398 <alisher12398@gmail.com>
2018-11-15 21:27:25 +0600

    commit2

--- app/src/main/java/com/android/android_app/Activity/LoginActivity.kt
+++ app/src/main/java/com/android/android_app/Activity/LoginActivity.kt
@@ -30,6 +30,7 @@ class LoginActivity : BaseActivity() {
     private lateinit var auth: FirebaseAuth
 //
     //
+    //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
