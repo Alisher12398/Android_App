d5e6d0b377af8cf4273854fdae1e1396798b81eb
Alisher12398 <Alisher12398@gmail.com>
2018-11-18 18:14:55 +0600

    MainActivity Bottom Navigation 1

--- app/src/main/java/com/android/android_app/activity/MainActivity.kt
+++ app/src/main/java/com/android/android_app/activity/MainActivity.kt
@@ -2,31 +2,23 @@ package com.android.android_app.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.android.android_app.R
 import android.widget.Toast
-
-
-
+import com.android.android_app.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*val adapter = PageAdapter(getSupportFragmentManager())
-        pager.adapter = adapter*/
-        //
-
     }
 
     var oneOnBackPressed = false
     private var back_pressed: Long = 0
     override fun onBackPressed() {
 
-       /*
+        /*
         Handler().postDelayed({
-            oneOnBackPressed = false
+        oneOnBackPressed = false
         }, 2000)*/
 
         if (back_pressed + 2000 > System.currentTimeMillis()) super.onBackPressed()
@@ -34,17 +26,4 @@ class MainActivity : AppCompatActivity() {
         back_pressed = System.currentTimeMillis()
 
     }
-
-   /* fun onClick(v: View) {
-        fTrans = fragmentManager.beginTransaction()
-        when (v.getId()) {
-            R.id.btnAdd -> fTrans.add(R.id.frgmCont, frag1)
-            R.id.btnRemove -> fTrans.remove(frag1)
-            R.id.btnReplace -> fTrans.replace(R.id.frgmCont, frag2)
-            else -> {
-            }
-        }
-        if (chbStack.isChecked()) fTrans.addToBackStack(null)
-        fTrans.commit()
-    }*/
 }
