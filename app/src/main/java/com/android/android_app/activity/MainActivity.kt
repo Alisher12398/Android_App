915cb18edf8e3d9fd84597193e1c1f9297b8125f
Alisher12398 <Alisher12398@gmail.com>
2018-11-19 23:46:37 +0600

    MainActivity Bottom Navigation 5 Change fragments

--- app/src/main/java/com/android/android_app/activity/MainActivity.kt
+++ app/src/main/java/com/android/android_app/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.android.android_app.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationMenu
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
 import android.widget.Toast
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottom_navigation.selectedItemId = R.id.action_fragment_main
+
 
         val cartFragment : Fragment = CartFragment()
         val favoritesFragment : Fragment = FavoritesFragment()
@@ -23,32 +28,47 @@ class MainActivity : AppCompatActivity() {
         val settingsFragment : Fragment = SettingsFragment()
 
 
-        val transaction : FragmentTransaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, mainFragment)
+        /*val transaction_mainFragment : FragmentTransaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, mainFragment)
+        val transaction_foodsFragment : FragmentTransaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, foodsFragment)
+        val transaction_favoritesFragment : FragmentTransaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, favoritesFragment)
+        val transaction_cartFragment : FragmentTransaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, cartFragment)
+        val transaction_settingsFragment : FragmentTransaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, settingsFragment)*/
         //transaction.replace(R.id.frame_layout, selectedFragment)
-        transaction.commit()
+        //transaction_mainFragment.commit()
 
-/*
+        supportFragmentManager.beginTransaction().add(R.id.frame_layout, mainFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.frame_layout, foodsFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.frame_layout, favoritesFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.frame_layout, cartFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.frame_layout, settingsFragment).commit()
+
+        var active = mainFragment
         var selectedFragment: Fragment? = null
-
         bottom_navigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.action_fragment_foods -> {
-                    fm.beginTransaction().hide(active).show(fragment1).commit();
-                    active = fragment1;
+                    supportFragmentManager.beginTransaction().hide(active).show(foodsFragment).commit()
+                    active = foodsFragment
                 }
                 R.id.action_fragment_favorites -> {
-                    selectedFragment = HomeFragment.newInstance();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content, selectedFragment).commit();
+                    supportFragmentManager.beginTransaction().hide(active).show(favoritesFragment).commit()
+                    active = favoritesFragment
                 }
                 R.id.action_fragment_main -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(mainFragment).commit()
+                    active = mainFragment
                 }
                 R.id.action_fragment_cart -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(cartFragment).commit()
+                    active = cartFragment
                 }
                 R.id.action_fragment_settings -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(settingsFragment).commit()
+                    active = settingsFragment
                 }
             }
             return@setOnNavigationItemSelectedListener true
-        }*/
+        }
 
     }
 
