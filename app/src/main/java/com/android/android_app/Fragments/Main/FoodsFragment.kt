bb96bac9af8b9268a06610c49d0305a3f25ff114
My Name <Alisher12398@gmail.com>
2018-11-20 18:37:23 +0600

    FoodsFragment Toolbar 1

--- app/src/main/java/com/android/android_app/Fragments/Main/FoodsFragment.kt
+++ app/src/main/java/com/android/android_app/Fragments/Main/FoodsFragment.kt
@@ -3,18 +3,30 @@ package com.android.android_app.Fragments.Main
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 
 import com.android.android_app.R
+import kotlinx.android.synthetic.*
 
 class FoodsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_foods, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_foods, container, false)
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar_foods_fragment)
+
+        return view
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 
 
