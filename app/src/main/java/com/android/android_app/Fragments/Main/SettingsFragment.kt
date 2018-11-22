e23c4d5caca25dc4a230b60ebc2d4d33e5cd2299
Maulyashev <pro100.maulyashev@gmail.com>
2018-11-22 21:40:39 +0600

    SettingsFragment

--- app/src/main/java/com/android/android_app/Fragments/Main/SettingsFragment.kt
+++ app/src/main/java/com/android/android_app/Fragments/Main/SettingsFragment.kt
@@ -6,15 +6,28 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 import com.android.android_app.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val textview_fragment_settings : TextView = view.findViewById(R.id.textview_fragment_settings)
+        val button_fragment_settings : Button = view.findViewById(R.id.button_fragment_settings)
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+
+        textview_fragment_settings.text = "Hello, ${currentUser?.email}"
+
+        return view
     }
 
 
